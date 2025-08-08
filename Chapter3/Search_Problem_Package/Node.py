e6ff6c260bf7b43b4c9aae17026efe350b38bf2b
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-08-08 15:42:34 +0300

    Change the cost attribute into path_cost for the node

--- Chapter3/Search_Problem_Package/Node.py
+++ Chapter3/Search_Problem_Package/Node.py
@@ -1,8 +1,8 @@
 class Node():
-  def __init__(self, state, parent = None, cost = 0):
+  def __init__(self, state, parent = None, path_cost = 0):
     self.state = state
     self.parent = parent
-    self.cost = cost
+    self.path_cost = path_cost
 
   def __str__(self):
-    return f"|Node|  State: {self.state} | Parent: {self.parent} | Cost: {self.cost}"
+    return f"|Node|  State: {self.state} | Parent: {self.parent} | Path Cost: {self.path_cost}"
