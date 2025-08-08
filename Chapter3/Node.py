a66fb768754e4c9fb07a0ceffe059601b0252ae4
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-08-08 15:29:11 +0300

    Add a __str__ method to the Node for easier printing

--- Chapter3/Node.py
+++ Chapter3/Node.py
@@ -3,3 +3,6 @@ class Node():
     self.state = state
     self.parent = parent
     self.cost = cost
+
+  def __str__(self):
+    return f"|Node|  State: {self.state} | Parent: {self.parent} | Cost: {self.cost}"
